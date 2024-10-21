a3d106b58f612c5f73f05e443c75f816a4b3a349
Matthew Zhu <47068683+mattz17@users.noreply.github.com>
2024-10-21 10:51:08 -0400

    update run_torch.py

--- project/run_torch.py
+++ project/run_torch.py
@@ -1,14 +1,15 @@
+# type: ignore
 import torch
 
 import minitorch
 
 
-def default_log_fn(epoch, total_loss, correct, losses): # type: ignore
+def default_log_fn(epoch, total_loss, correct, losses):
     print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
 
 
 class Network(torch.nn.Module):
-    def __init__(self, hidden_layers): # type: ignore
+    def __init__(self, hidden_layers):
         super().__init__()
 
         # Submodules
@@ -16,39 +17,39 @@ class Network(torch.nn.Module):
         self.layer2 = Linear(hidden_layers, hidden_layers)
         self.layer3 = Linear(hidden_layers, 1)
 
-    def forward(self, x): # type: ignore
+    def forward(self, x):
         h = self.layer1.forward(x).relu()
         h = self.layer2.forward(h).relu()
         return self.layer3.forward(h).sigmoid()
 
 
 class Linear(torch.nn.Module):
-    def __init__(self, in_size, out_size): # type: ignore
+    def __init__(self, in_size, out_size):
         super().__init__()
         self.weight = torch.nn.Parameter(2 * (torch.rand((in_size, out_size)) - 0.5))
         self.bias = torch.nn.Parameter(2 * (torch.rand((out_size,)) - 0.5))
 
-    def forward(self, x): # type: ignore
+    def forward(self, x):
         return x @ self.weight + self.bias
 
 
 class TorchTrain:
-    def __init__(self, hidden_layers): # type: ignore
+    def __init__(self, hidden_layers):
         self.hidden_layers = hidden_layers
         self.model = Network(hidden_layers)
 
-    def run_one(self, x): # type: ignore
+    def run_one(self, x):
         return self.model.forward(torch.tensor([x]))
 
-    def run_many(self, X): # type: ignore
+    def run_many(self, X):
         return self.model.forward(torch.tensor(X)).detach()
 
     def train(
         self,
-        data, # type: ignore
-        learning_rate, # type: ignore
-        max_epochs=500, # type: ignore
-        log_fn=default_log_fn, # type: ignore
+        data,
+        learning_rate,
+        max_epochs=500,
+        log_fn=default_log_fn,
     ):
         self.model = Network(self.hidden_layers)
         self.max_epochs = max_epochs
